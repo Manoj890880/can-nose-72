9bd44d396ec204b6b476d377391790edd1c38674
manoj <manojsahoo890880@gmail.com>
2023-02-22 23:23:19 +0530

    assed

--- WATCHMAN/src/com/watchman/exception/SomeThingWrongException.java
+++
@@ -1,8 +0,0 @@
-package com.watchman.exception;
-
-public class SomeThingWrongException extends Exception{
-	@Override
-	public String toString() {
-		return "Something Went Wrong , Please try again later";
-	}
-}
